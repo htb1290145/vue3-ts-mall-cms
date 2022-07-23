ca282e94e7c6854738472c8e7d0c1d41d3497732
物华 <10572332+wuhua11@user.noreply.gitee.com>
2022-07-23 17:22:13 +0800

    'axios封装完成'

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -11,12 +11,14 @@ class HTBRequest {
   showLoading: boolean
   loading?: any
 
-  // 1.constructor每次new时都会执行，config中取出的拦截器是实例拦截器
   constructor(config: HTBRequestConfig) {
+    // 创建axios实例
     this.instance = axios.create(config)
+    // 保存基本信息。拦截器、显示loading
     this.interceptors = config.interceptors
     this.showLoading = config.showLoading ?? DEFAULT_LOADING
 
+    // 1.constructor每次new时都会执行，config中取出的拦截器是实例拦截器
     this.instance.interceptors.request.use(
       this.interceptors?.requestInterceptor,
       this.interceptors?.requestInterceptorCatch
@@ -31,7 +33,6 @@ class HTBRequest {
       (config) => {
         console.log('所有的请求都有的请求拦截器')
         console.log(this.showLoading)
-
         if (this.showLoading) {
           // ElLoading
           this.loading = ElLoading.service({
@@ -60,7 +61,7 @@ class HTBRequest {
     )
   }
 
-  request<T>(config: HTBRequestConfig): Promise<T> {
+  request<T>(config: HTBRequestConfig<T>): Promise<T> {
     return new Promise((resolve, reject) => {
       // 3.某个实例某个请求的拦截器。从config取出的。
       if (config.interceptors?.requestInterceptor) {
@@ -70,6 +71,25 @@ class HTBRequest {
       if (config.showLoading === false) {
         this.showLoading = config.showLoading
       }
+      // this.instance
+      // .request<any, T>(config)
+      // .then((res) => {
+      //   // 1.单个请求对数据的处理
+      //   if (config.interceptors?.responseInterceptor) {
+      //     res = config.interceptors.responseInterceptor(res)
+      //   }
+      //   // 2.将showLoading设置true, 这样不会影响下一个请求
+      //   this.showLoading = DEAFULT_LOADING
+
+      //   // 3.将结果resolve返回出去
+      //   resolve(res)
+      // })
+      // .catch((err) => {
+      //   // 将showLoading设置true, 这样不会影响下一个请求
+      //   this.showLoading = DEAFULT_LOADING
+      //   reject(err)
+      //   return err
+      // })
       // 请求的数据处理
       this.instance.request<any, T>(config).then(
         (res) => {
@@ -78,7 +98,7 @@ class HTBRequest {
           }
           // 设置回初始值，不会影响下一个请求
           this.showLoading = DEFAULT_LOADING
-          // 请求成功的操作
+          // 请求成功,把结果resolve返回出去
           resolve(res)
         },
         (err) => {
@@ -93,19 +113,19 @@ class HTBRequest {
     })
   }
 
-  get<T>(config: HTBRequestConfig): Promise<T> {
+  get<T>(config: HTBRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'GET' })
   }
 
-  post<T>(config: HTBRequestConfig): Promise<T> {
+  post<T>(config: HTBRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'POST' })
   }
 
-  delete<T>(config: HTBRequestConfig): Promise<T> {
+  delete<T>(config: HTBRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'DELETE' })
   }
 
-  patch<T>(config: HTBRequestConfig): Promise<T> {
+  patch<T>(config: HTBRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'PATCH' })
   }
 }
--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -4,16 +4,17 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 export interface HTBRequestInterceptors<T = AxiosResponse> {
   // 请求拦截
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
-  requestInterceptorCatch?: (error: T) => T
+  requestInterceptorCatch?: (error: any) => any
   // 响应拦截
   // responseInterceptor?: (res: AxiosResponse) => AxiosResponse
-  responseInterceptor?: (res: any) => any
-  responseInterceptorCatch?: (error: T) => T
+  responseInterceptor?: (res: T) => T
+  responseInterceptorCatch?: (error: any) => any
 }
 
 // config接口
 // HTBRequestConfig 继承自 AxiosRequestConfig——扩展了自己的拦截器
-export interface HTBRequestConfig extends AxiosRequestConfig {
-  interceptors?: HTBRequestInterceptors
+export interface HTBRequestConfig<T = AxiosResponse>
+  extends AxiosRequestConfig {
+  interceptors?: HTBRequestInterceptors<T>
   showLoading?: boolean
 }
